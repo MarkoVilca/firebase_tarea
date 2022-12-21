019e816b9b00bc948351842b1b217804ebd642eb
Marco <garishito123@outlook.com>
2022-12-20 21:01:53 -0500

    quinto commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,107 +22,10 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Firebase Firestore"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: (() {
-                  tasksReference.get().then((value) {
-                    //QuerySnapshot collection = value;
-                    //List<QueryDocumentSnapshot> docs = collection.docs;
-                    //QueryDocumentSnapshot doc = docs[1];
-                    //print(doc.id);
-                    //print(doc.data());
-
-                    QuerySnapshot collection = value;
-                    collection.docs.forEach((QueryDocumentSnapshot element) {
-                      Map<String, dynamic> myMap =
-                          element.data() as Map<String, dynamic>;
-                      print(myMap["title"]);
-                    });
-                  });
-                }),
-                child: Text("Obtener la data"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  tasksReference.add(
-                    {
-                      "title": "Ir de compras al super 2",
-                      "descripcion":
-                          "Debemos de comprar comdia para todoo el mes",
-                    },
-                  ).then((DocumentReference value) {
-                    print(value.id);
-                  }).catchError((error) {
-                    print("Ocurrio un error en el registro");
-                  }).whenComplete(() {
-                    print("El registro ha terminado");
-                  });
-                },
-                child: Text("Agregar Documento"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  tasksReference.doc("CWJi2ClP3Tm3wnOSvkEa").update(
-                    {
-                      "title": "Ir de paseo",
-                      "descripcion": "Tenems que salir muy temprano",
-                    },
-                  ).catchError(
-                    (error) {
-                      print(error);
-                    },
-                  ).whenComplete(
-                    () {
-                      print("Actualizacion terminada");
-                    },
-                  );
-                },
-                child: Text("Actualizar documento"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  tasksReference
-                      .doc("DAzrwsO00QPMHhRXIR8x")
-                      .delete()
-                      .catchError(
-                    (error) {
-                      print(error);
-                    },
-                  ).whenComplete(
-                    () {
-                      print("La eliminacion esta completa");
-                    },
-                  );
-                },
-                child: Text("Eliminar documento"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  tasksReference.doc("A00001").set(
-                    {
-                      "title": "Ir al conciertoo",
-                      "descripcion":
-                          "Este fin de semana debemos ir al concierto",
-                    },
-                  ).catchError(
-                    (error) {
-                      print("Creacioon completada");
-                    },
-                  );
-                },
-                child: Text(
-                  "Agregar documento personalizado",
-                ),
-              ),
-            ],
-          ),
-          body: StreamBuilder(
+        body: StreamBuilder(
             stream: counter(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasData) {
+            builder: (BuildContext context, AsyncSnapshot snapshot){
+              if(snapshot.hasData) {
                 int data = snapshot.data;
                 return Center(
                   child: Text(
@@ -130,13 +33,32 @@ class HomePage extends StatelessWidget {
                     style: TextStyle(fontSize: 50),
                   ),
                 );
-              }
-              return Center(
-                child: CircularProgressIndicator(),
-            );
-          },
-        ),
-      )
-    );
-  }
+                stream:
+                tasksReference.snapshots();
+                builder:
+                (BuildContext context, AsyncSnapshot snap) {
+                  if (snap.hasData) {
+                    QuerySnapshot collection = snap.data;
+                    List<QueryDocumentSnapshot> docs = collection.docs;
+                    List<Map<String, dynamic>> docsMap = docs
+                        .map((e) => e.data() as Map<String, dynamic>)
+                        .toList();
+                    print(docsMap);
+                    return ListView.builder(
+                      itemCount: docsMap.length,
+                      itemBuilder: (BuildContext contex, int index) {
+                        return ListTile(
+                          title: Text(docsMap[index]["title"]),
+                        );
+                      },
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+      );
+     };
+    } 
+  })
+  );
+ }
 }
